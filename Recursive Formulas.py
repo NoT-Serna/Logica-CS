d1a3888399379731a709aab9341f780dacb7ef73
Serno uwu <jserna7856@gmail.com>
2022-08-04 19:23:00 -0500

    New functions

--- Recursive Formulas.py
+++ Recursive Formulas.py
@@ -4,6 +4,34 @@ class Formula():
     def __init__(self):
         pass
 
+#Methods
+    def __str__(self):
+        if type(self) == Letter:
+            return self.letter
+        
+        elif type(self) == Negation:
+            return "-" + str(self.subf)
+        
+        elif type(self) == Binary:
+            return "(" + str(self.left) + self.connector + str(self.right) + ")"
+        
+
+    def num_connec(self):
+        if type(self) == Letter:
+            return 0
+        elif type(self) == Negation:
+            return 1 + self.subf.num_connec()
+        elif type(self) == Binary:
+            return 1 + self.left.num_connec() + self.right.num_connec()
+        
+    def num_paren(self):
+        if type(self) == Letter:
+            return 0
+        elif type(self) == Negation:
+            return 0 + self.subf.num_paren()
+        elif type(self) == Binary:
+            return 2 + self.left.num_paren() + self.right.num_paren()
+
 class Letter(Formula):
     def __init__(self, letter):
         self.letter = letter
@@ -20,31 +48,20 @@ class Binary(Formula):
         self.left = left
         self.right = right
 
-#Methods
-    def __str__(self):
-        if type(self) == Letter:
-            return self.letter
-        
-        elif type(self) == Negation:
-            return "-" + str(self.subf)
-        
-        elif type(self) == Binary:
-            return "(" + str(self.left) + self.connector + str(self.right) + ")"
-        
-    setattr(Formula, "__str__", __str__)
-
-    def num_connec(self):
-        if type(self) == Letter:
-            return 0
-        elif type(self) == Negation:
-            return 1 + self.subf.num_connec()
-        elif type(self) == Binary:
-            return 1 + self.left.num_connec() + self.right.num_connec()
 
 
 if __name__ == "__main__":
+    # Ejercicio 1
+    print("Ejercicio 1")
     p = Letter("p")
     print("Varaible" , p)
+
+    s = Letter("s")
+    print("Variable", s)
+
+    r = Letter("r")
+    print("Variable", r)
+    
     q = Letter("q")
     print("Varaible" , q)
 
@@ -57,7 +74,17 @@ if __name__ == "__main__":
     a3 = Negation(Binary("Y",p,Negation(q)))
     print( "Formula #3 " , a3)
 
+    a4 = Binary('O', Binary('>', Binary('Y', Letter(p), Negation(Letter(s))), Binary('Y', Letter(p), Negation(Letter(r)))), Binary('O', Negation(Letter(q)), Letter(p)))
+    print(a4)
+
+   #print("There are " ,a1.num_connec(), " connectors in the second formula ")
    # print("There are " ,a2.num_connec(), " connectors in the second formula ")
-   # print("There are " ,a3.num_connec(), " connectors in the third formula ")
+    print("There are " ,a3.num_connec(), " connectors in the third formula ")
+  
+    
+
+   # print("There are " ,a1.num_paren(), " parenthesis in the second formula ")
+   # print("There are " ,a2.num_paren(), " parenthesis in the second formula ")
+   # print("There are " ,a3.num_paren(), " parenthesis in the third formula ")
     
 
