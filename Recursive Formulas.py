621d279f0619f5fc67b11a41a97e5eaffbfe7b28
Serno uwu <jserna7856@gmail.com>
2022-08-08 21:34:02 -0500

    More Methods

--- Recursive Formulas.py
+++ Recursive Formulas.py
@@ -31,6 +31,22 @@ class Formula():
             return 0 + self.subf.num_paren()
         elif type(self) == Binary:
             return 2 + self.left.num_paren() + self.right.num_paren()
+    
+    def num_letras(self):
+        if type(self) == Letter:
+            return 1
+        elif type(self) == Negation:
+            return 0+ self.subf.num_letras()
+        elif type(self) == Binary:
+            return self.left.num_letras() + self.right.num_letras()
+    
+    def num_bin(self):
+        if type(self) == Letter:
+            return 0
+        elif type(self) == Negation:
+            return 0 + self.subf.num_bin()
+        elif type(self) == Binary:
+            return 1 + self.left.num_bin() + self.right.num_bin()
 
 class Letter(Formula):
     def __init__(self, letter):
@@ -74,17 +90,26 @@ if __name__ == "__main__":
     a3 = Negation(Binary("Y",p,Negation(q)))
     print( "Formula #3 " , a3)
 
-    a4 = Binary('O', Binary('>', Binary('Y', Letter(p), Negation(Letter(s))), Binary('Y', Letter(p), Negation(Letter(r)))), Binary('O', Negation(Letter(q)), Letter(p)))
-    print(a4)
+    a4 = Binary("Y",p,q)
+    print("Formula #4 ", a4 )
 
-   #print("There are " ,a1.num_connec(), " connectors in the second formula ")
-   # print("There are " ,a2.num_connec(), " connectors in the second formula ")
-    print("There are " ,a3.num_connec(), " connectors in the third formula ")
+    a5 = Negation(Binary("Y",p,q))
+    print("Formula #5 ", a5)
+
+
+
+    #print("There are " ,a1.num_connec(), " connectors in the second formula ")
+    #print("There are " ,a2.num_connec(), " connectors in the second formula ")
+    #print("There are " ,a3.num_connec(), " connectors in the third formula ")
+    #print(a4.num_connec())
+    #print(a5.num_connec())
+    print(a4.num_bin())
+
   
     
 
-   # print("There are " ,a1.num_paren(), " parenthesis in the second formula ")
-   # print("There are " ,a2.num_paren(), " parenthesis in the second formula ")
-   # print("There are " ,a3.num_paren(), " parenthesis in the third formula ")
+    #print("There are " ,a1.num_paren(), " parenthesis in the second formula ")
+    #print("There are " ,a2.num_paren(), " parenthesis in the second formula ")
+    #print("There are " ,a3.num_paren(), " parenthesis in the third formula ")
     
 
